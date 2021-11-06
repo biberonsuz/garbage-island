4394b19e579d72e4af2328722c15cd8082ba0b1d
biberonsuz <pinarkzk@gmail.com>
2021-11-06 22:39:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const express = require("express")
+
+// create an express app
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("public", { fallthrough: false }))
+
+// define the first route
+app.get("/", function (req, res) {})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 80, 
+	() => console.log("Server is running..."));
+
+
+
+
